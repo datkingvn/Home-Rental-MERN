72c75d47d5a01d16a935f9c637303046e30880ae
Mạnh Đạt <tranhuudats1@gmail.com>
2024-01-30 08:58:01 +0700

    Base Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <div>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/dang-nhap" element={<LoginPage />}/>
+          <Route path="/dang-ky" element={<RegisterPage />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
